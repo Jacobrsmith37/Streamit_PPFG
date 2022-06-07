4b50b3a50fdd056c5645ecd1b5b6de8fc0f15143
Jacob Smith <77115241+Jacobrsmith37@users.noreply.github.com>
2022-06-07 12:07:11 -0500

    Update Streamlit_v6.py

--- Streamlit_v6.py
+++ Streamlit_v6.py
@@ -50,7 +50,7 @@ df_HS.rename(columns={'OVBD_PSI': 'HS_PSI'}, inplace=True)
 
 
 # Bring in topset and remove last column
-Topset1 = pd.read_csv(r'C:\Users\jac84753\OneDrive\Documents\Jacob\HESS\Pore Pressure\Updated_PPFG\Topset1_Updated_PPFG.txt', sep = "\t")
+Topset1 = pd.read_csv('Topset1_Updated_PPFG.csv', sep = "\t")
 Topset1.drop('GeosteeringDip', axis = 1, inplace = True)
 
 Topset1['TopName'] = Topset1['TopName'].str.title()
