7af557d033a5e240bc1be550c23b439cf8f766ce
Jacob Smith <77115241+Jacobrsmith37@users.noreply.github.com>
2022-06-08 12:43:35 -0500

    Update Streamlit_v9.py

--- Streamlit_v9.py
+++ Streamlit_v9.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 import lasio
+import pillow as PIL
 from PIL import Image
 
 image_logo = Image.open('Hess_Logo.jpg')
