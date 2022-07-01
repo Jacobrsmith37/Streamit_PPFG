a2c53ea7f5b51141b668fc5a857db5892cc74f7a
Jacob Smith <77115241+Jacobrsmith37@users.noreply.github.com>
2022-07-01 12:44:14 -0500

    Update Home_Page_v1.py

--- Home_Page_v1.py
+++ Home_Page_v1.py
@@ -115,6 +115,47 @@ def home():
     st.image(image_bakken_rig)
     st.subheader('To begin, upload a Topset csv file on the Sidebar')
 
+options = st.sidebar.radio('View Calulator', ['Yes', 'No'])
+def calc():
+
+    st.write('Mud Weight to PSI Calculator')
+
+
+    def ppg_to_psi():
+        st.session_state.psi = st.session_state.ppg * st.session_state.depth * .052
+
+
+
+    def psi_to_ppg():
+        st.session_state.ppg = st.session_state.psi / st.session_state.depth / .052
+
+
+    def depth():
+        st.session_state.depth 
+
+
+    def clear():
+        st.session_state.depth, st.session_state.psi, st.session_state.ppg = 0,0,0
+
+
+    col1, buff, col2 = st.columns([2,1,2])
+    with col1:
+        Depth = st.number_input('Depth (ft)' , key = 'depth')
+
+
+    with st.container():   
+        col1, buff, col2 = st.columns([2,1,2])
+        with col1:
+            Mud = st.number_input('Mud Weight (PPG)' , key = 'ppg',
+                                 on_change = ppg_to_psi)
+
+
+
+        with col2:
+            Pressure = st.number_input('Formation Pressure (PSI):' , key = 'psi',
+                                      on_change = psi_to_ppg)    
+
+    st.button('Clear Inputs', on_click = clear)        
 
 
     
@@ -975,5 +1016,9 @@ def data_summary():
 
 
 home()
+if options == 'Yes':
+    calc()
+else:
+    ''
 interactive_plot()
 data_summary()
