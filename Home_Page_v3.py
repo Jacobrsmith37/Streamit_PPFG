68344f1f9736b9e060e88fb7549ddd78e8b06c51
Jacob Smith <77115241+Jacobrsmith37@users.noreply.github.com>
2023-02-28 14:29:45 -0600

    Update Home_Page_v3.py

--- Home_Page_v3.py
+++ Home_Page_v3.py
@@ -991,7 +991,7 @@ def interactive_plot():
     plot.add_trace(go.Scatter(x = df_ovbd['HS_FG_PPG'], y = df_ovbd['DEPTH'], name = 'HS Frac Gradient', marker_color ="#FF4040"))
    
     #added 02/23/2023
-    #plot.add_trace(go.Scatter(x = df_dfit['PPG'], y = df_dfit['Depth (TVD)'], name = 'DFIT Data Point', mode='markers', marker_color ="#000000"))
+    plot.add_trace(go.Scatter(x = df_dfit['PPG'], y = df_dfit['Depth (TVD)'], name = 'DFIT Data Point', mode='markers', marker_color ="#000000"))
     
     
     plot.update_yaxes(nticks = 30, range = [max(new_df['TVD']) + 500, 0]) #, autorange = 'reversed')
