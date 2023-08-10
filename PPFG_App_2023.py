d88ed7c3c9647802d8e24249649190e1d9d07abc
Jacob Smith <77115241+Jacobrsmith37@users.noreply.github.com>
2023-08-10 12:40:17 -0500

    Update PPFG_App_2023.py

--- PPFG_App_2023.py
+++ PPFG_App_2023.py
@@ -1044,11 +1044,11 @@ def interactive_plot():
     
     
 #     plot.add_hline(y = int(pierre_upload['TVD']), line_dash="dot", row=1, col="all", annotation_text = 'Pierre', annotation_position = 'top left')
-    plot.add_hline(y = int(greenhorn_upload['TVD']), line_dash="dot", row=1, col="all", annotation_text = 'GREENHORN', annotation_xshift = '4', annotation_position = 'top left')
+    plot.add_hline(y = int(greenhorn_upload['TVD']), line_dash="dot", row=1, col="all", annotation_text = 'GREENHORN', annotation_xshift = 4, annotation_position = 'top left')
 #     plot.add_hline(y = int(greenhorn_upload['TVD']) + 200, line_dash="dot", row=1, col="all", annotation_text = 'Greenhorn Silt', annotation_position = 'top left')
 #     plot.add_hline(y = int(mowry_upload['TVD']), line_dash="dot", row=1, col="all", annotation_text = 'Mowry', annotation_position = 'top left')
     plot.add_hline(y = int(inyan_kara_upload['TVD']), line_dash="dot", row=1, col="all", annotation_text = 'INYAN_KARA',annotation_xshift = 8, annotation_position = 'top left')
-    plot.add_hline(y = int(spearfish_upload['TVD']), line_dash="dot", row=1, col="all", annotation_text = 'SPEARFISH',annotation_xshift = 4, annotation_position = 'top left')
+    plot.add_hline(y = int(spearfish_upload['TVD']), line_dash="dot", row=1, col="all", annotation_text = 'SPEARFISH',annotation_xshift = 12, annotation_position = 'top left')
     plot.add_hline(y = int(swift_upload['TVD']), line_dash="dot", row=1, col="all", annotation_text = 'SWIFT', annotation_xshift = 4,annotation_position = 'top left')
     plot.add_hline(y = int(rierdon_upload['TVD']), line_dash="dot", row=1, col="all", annotation_text = 'RIERDON', annotation_xshift = 4,annotation_position = 'top left')
     plot.add_hline(y = int(amsden_upload['TVD']), line_dash="dot", row=1, col="all", annotation_text = 'AMSDEN',annotation_xshift = 4, annotation_position = 'top left')
