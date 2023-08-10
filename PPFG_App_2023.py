b94b0f90b4df877b111015d7fe93a40b3101f9d8
Jacob Smith <77115241+Jacobrsmith37@users.noreply.github.com>
2023-08-10 10:06:00 -0500

    Update PPFG_App_2023.py

--- PPFG_App_2023.py
+++ PPFG_App_2023.py
@@ -995,7 +995,7 @@ def interactive_plot():
         dict(
             source = image_logo,
             xref = "paper", yref="paper",
-            x=0, y=1.05,
+            x= 0, y=1.1,
             sizex=0.2, sizey=0.2,
             xanchor="left", yanchor="top"
         )
