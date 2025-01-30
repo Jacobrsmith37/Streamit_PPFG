f0ada1f1e00251efdb033d8fd33caa7a8122701c
Jacob Smith <77115241+Jacobrsmith37@users.noreply.github.com>
2025-01-30 13:10:51 -0600

    Update PPFG_App_2023.py

--- PPFG_App_2023.py
+++ PPFG_App_2023.py
@@ -1050,7 +1050,7 @@ def interactive_plot():
 
     plot.update_layout(annotations =[dict(
                                       x = .8,
-                                      y = -.04,
+                                      y = -.06,
                                       xref = 'paper',
                                       yref = 'paper',
                                       text = ('Analyst: ' + name_input ))])
