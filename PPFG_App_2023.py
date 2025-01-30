dbdc16a92aa2f1c1565f2e9b02ced68c382f4f64
Jacob Smith <77115241+Jacobrsmith37@users.noreply.github.com>
2025-01-30 12:01:52 -0600

    Update PPFG_App_2023.py

--- PPFG_App_2023.py
+++ PPFG_App_2023.py
@@ -1012,7 +1012,7 @@ def interactive_plot():
 
     name_input = st.sidebar.text_input('Enter Analyst Name' ,'')
     today = datetime.datetime.now()
-    date = st.date_input(today)
+    date = st.date_input(today, format = "MM.DD.YYY")
 
     # Add image
     plot.add_layout_image(
