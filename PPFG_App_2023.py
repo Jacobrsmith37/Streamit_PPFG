0ecdcd927c959bf7db8097c5d7d60c6bfcd5bb41
Jacob Smith <77115241+Jacobrsmith37@users.noreply.github.com>
2025-01-30 12:42:44 -0600

    Update PPFG_App_2023.py

--- PPFG_App_2023.py
+++ PPFG_App_2023.py
@@ -1014,7 +1014,7 @@ def interactive_plot():
     name_input = st.sidebar.text_input('Enter Analyst Name' ,'')
 
     
-    plot.text_input(0.5, -0.1, f"Date: {datetime.today().strftime('%m-%d-%Y')}", ha = 'center' , va = 'top', fontsize = 12 )
+    plot.text_input(Date, value = {datetime.today().strftime('%m-%d-%Y')}")
   
     # Add image
     plot.add_layout_image(
